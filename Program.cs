c68aee8f40682f03081e22ed1ec7c2ee6129ff47
Vas <VasVadum@Gmail.com>
2022-08-05 18:37:11 -0400

    Nearly Finished

--- Program.cs
+++ Program.cs
@@ -38,6 +38,9 @@ namespace IngameScript
             if (StandardBatteries == null) { Echo("Battery group not found!"); return; }
             StandardBatteries.GetBlocksOfType(myBatteryBlocks);
 
+            var eBat = (IMyBatteryBlock)GridTerminalSystem.GetBlockWithName("Emergency Battery");
+            if (eBat == null) { Echo("Emergency battery not found. Please name a single battery 'Emergency battery'."); return; }
+
             bool chargeStatus = false;
             float currentStoredPowerAll = 0f;
             float maxStoredPowerAll = 0f;
@@ -61,9 +64,11 @@ namespace IngameScript
             }
             if (underControl) {
                 Echo("Vehicle is currently under control.");
-                // Set e.battery to recharge
-                // Set batteries to auto
-                // Set gas tanks on
+                eBat.ChargeMode = ChargeMode.Recharge;
+                foreach (var battery in myBatteryBlocks) { battery.ChargeMode = ChargeMode.Auto; }
+                List<IMyGasTank> myGasTanks = new List<IMyGasTank>();
+                GridTerminalSystem.GetBlocksOfType(myGasTanks);
+                if (myGasTanks != null) { foreach (var gasTank in myGasTanks) { gasTank.Enabled = true; } }
             } else if (chargeStatus) {
                 if (EmBatteryPercent < 0.9 && percentStoredPower < 0.3) {
                     Echo("Vehicle is currently in recharge mode.");
@@ -72,43 +77,64 @@ namespace IngameScript
                 }
             } else {
                 if (percentStoredPower <= 0.05 && EmBatteryPercent <= 0.1) {
-                    // Shutdown Everything.
-                    // Enable batteries recharge.
+                    // Shutdown Everything Remaining (Survival Kit?)
+                    foreach (var battery in myBatteryBlocks) { battery.ChargeMode = ChargeMode.Recharge; }
                     chargeStatus = true;
                     Echo("Emergency Mode: Full system shutdown.");
                 } else if (percentStoredPower <= 0.05 && EmBatteryPercent <= 0.4) {
-                    // Turn Beacon Off
-                    // Reset Beacon Name
+                    List<IMyBeacon> myBeacons = new List<IMyBeacon>();
+                    GridTerminalSystem.GetBlocksOfType(myBeacons);
+                    if (myBeacons != null) { foreach (var beacon in myBeacons) { beacon.Enabled = false; beacon.DisplayName = ("Spawn Rover"); } }
+                    // /\ Not sure if this is right either. Needs to reset the display name of the beacon.
                     Echo("Emergency Mode: System backup power extremely low.");
                 } else if (percentStoredPower <= 0.05 && EmBatteryPercent <= 0.7) {
                     // Disable everything except survival kit.
-                    // Set Beacon Name to Emergency Mode.
+                    List<IMyBeacon> myBeacons = new List<IMyBeacon>();
+                    GridTerminalSystem.GetBlocksOfType(myBeacons);
+                    if (myBeacons != null) { foreach (var beacon in myBeacons) { beacon.Enabled = true; beacon.DisplayName = ("Spawn Rover: Emergency! Power: " + (EmBatteryPercent * 100) + "%"); } }
+                    // /\ Not sure if this is right either. Needs to display the current percent of emergency power in the beacon name.
                     Echo("Emergency Mode: System backup power low.");
                 } else if (percentStoredPower <= 0.05 && EmBatteryPercent > 0.7) { // Potential risk of all false here.
-                    // Enable Emergency Batteries.
-                    // Set Batteries to Discharge.
+                    eBat.ChargeMode = ChargeMode.Auto;
+                    foreach (var battery in myBatteryBlocks) { battery.ChargeMode = ChargeMode.Discharge; }
                     Echo("System power is extremely low, switching to Emergency battery.");
                 } else if (percentStoredPower <= 0.25 && percentStoredPower > 0.05) {
-                    // Shutdown Lights
-                    // Shutdown LCDs
-                    // Shutdown Sensors
-                    // Shutdown Gas tanks
-                    // Park if not parked
-                    // Set e.bat to Recharge
+                    List<IMyLightingBlock> myLights = new List<IMyLightingBlock>();
+                    GridTerminalSystem.GetBlocksOfType(myLights);
+                    if (myLights != null) { foreach (var light in myLights) { light.Enabled = false; } }
+                    List<IMyTextPanel> myLCDs = new List<IMyTextPanel>();
+                    GridTerminalSystem.GetBlocksOfType(myLCDs);
+                    if (myLCDs != null) { foreach (var lcd in myLCDs) { lcd.Enabled = false; } }
+                    List<IMySensorBlock> mySensors = new List<IMySensorBlock>();
+                    GridTerminalSystem.GetBlocksOfType(mySensors);
+                    if (mySensors != null) { foreach (var sensor in mySensors) { sensor.Enabled = false; } }
+                    List<IMyGasTank> myGasTanks = new List<IMyGasTank>();
+                    GridTerminalSystem.GetBlocksOfType(myGasTanks);
+                    if (myGasTanks != null) { foreach (var gasTank in myGasTanks) { gasTank.Enabled = false; } }
+                    if (cockpits != null) {foreach (var cockpit in cockpits) { cockpit.HandBrake = true; } } // Not happy with this.
+                    eBat.ChargeMode = ChargeMode.Recharge;
                     Echo("System power is low.");
                 } else if (percentStoredPower <= 0.5 && percentStoredPower > 0.25) {
-                    // Shutdown Antennas
+                    List<IMyRadioAntenna> myAntennas = new List<IMyRadioAntenna>();
+                    GridTerminalSystem.GetBlocksOfType(myAntennas);
+                    if (myAntennas != null) { foreach (var antenna in myAntennas) { antenna.Enabled = false; } }
+                    List<IMyOreDetector> myOreDetectors = new List<IMyOreDetector>();
+                    GridTerminalSystem.GetBlocksOfType(myOreDetectors);
+                    if (myOreDetectors != null) { foreach (var oreDetector in myOreDetectors) { oreDetector.Enabled = false; } }
                     // Shutdown Heat Exhcnagers (If they exist)
-                    // Shutdown Ore Detectors
-                    // Set e.bat to Recharge
+                    eBat.ChargeMode = ChargeMode.Recharge;
                     Echo("System power is reduced.");
                 } else if (percentStoredPower <= 0.75 && percentStoredPower > 0.5) {
-                    // Shutdown Oxygen Generators
-                    // Shutdown Vents
-                    // Set e.bat to Recharge
+                    List<IMyGasGenerator> myGasGenerators = new List<IMyGasGenerator>();
+                    GridTerminalSystem.GetBlocksOfType(myGasGenerators);
+                    if (myGasGenerators != null) { foreach (var gasGen in myGasGenerators) { gasGen.Enabled = false; } }
+                    List<IMyAirVent> myVents = new List<IMyAirVent>();
+                    GridTerminalSystem.GetBlocksOfType(myVents);
+                    if (myVents != null) { foreach (var vent in myVents) { vent.Enabled = false; } }
+                    eBat.ChargeMode = ChargeMode.Recharge;
                     Echo("System power is nominal.");
                 } else if (percentStoredPower >= 0.75) {
-                    // Set e.bat to Recharge
+                    eBat.ChargeMode = ChargeMode.Recharge;
                     Echo("System power is great.");
                 } else {
                     Echo("Something has gone wrong, I will wait for something to change.");
@@ -117,6 +143,3 @@ namespace IngameScript
         }
     }
 }
-
-// "[1] Emergency Battery" (Will rename if script works)
-// Group Names: Batteries, Exterior Lights, Floodlights, Hydrogen Tanks, Wheels
